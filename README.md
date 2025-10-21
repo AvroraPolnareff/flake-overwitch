2cde5d21f75d5803ab1de1b1e304294c222d5bfe
Rutger Bazen <hello@rbdm.nl>
2025-10-21 21:38:01 +0200

    Update to release 2.1.1
    Update Readme.md
    Remove overwitch systemd service definition
    Updated flake.lock

--- README.md
+++ README.md
@@ -1,26 +1,34 @@
 # Nixos Flake for Dagargo's Overwitch
+
+For more information about Overwitch:
 - https://github.com/dagargo/overwitch
 
-This flake builds Overwitch v2.1 from source and installs the required udev rules.
+This Nixos Flake fetches the (hopefully) latest release of Dagargo's repo.
+Compiles from source, adds the udev rules for the Elektron machines, and registers a dbus service.
 
-Comes with a devShell.
+For more information on the dbus service, see https://github.com/dagargo/overwitch/issues/78
+
+A devShell is also provided.
+
 
 Based on the nixpkgs work done by https://github.com/dag-h
 
 # Usage:
-Include the flake in your `flake.nix`
+Include this flake in your `flake.nix` inputs: 
 ```
-   inputs.overwitch = {
-       url = "github:Are10/flake-overwitch/main";
-       inputs.nixpkgs.follows = "nixpkgs";
+  inputs.overwitch = {
+    url = "github:Are10/flake-overwitch/dbus";
+    inputs.nixpkgs.follows = "nixpkgs";
   };
-
+```
+add the nixos module to your modules:
+```
   modules = [
     inputs.overwitch.nixosModules.default
   ];
 ```
-
-And then enable it in `configuration.nix`
+And then enable it in (for example) `configuration.nix`
 ```   
 services.overwitch.enable = true; 
 ```
+
--- flake.nix
+++ flake.nix
@@ -2,93 +2,99 @@
   description = "A flake for building Overwitch";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: 
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      packages = rec {
-        overwitch = pkgs.stdenv.mkDerivation {
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = rec {
+          overwitch = pkgs.stdenv.mkDerivation {
 
-          name = "overwitch";
+            name = "overwitch";
 
-          src = pkgs.fetchzip {
-            url = "https://github.com/dagargo/overwitch/releases/download/2.1/overwitch-2.1.tar.gz";
-            hash = "sha256-LBDlfMEBuEZAROpou2tCQ4hDcGDVmxU5AUveKPORIYc=";
+            src = builtins.fetchGit {
+	      url = "https://github.com/dagargo/overwitch";
+	      ref = "refs/tags/2.1.1";
+	      rev = "230a05255adb6c656b432b7a2eb3be899072c56e";
+            };
+
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+              autoreconfHook
+              wrapGAppsHook3
+            ];
+
+            buildInputs = with pkgs; [
+              libtool
+              libusb1
+              libjack2
+              libsamplerate
+              libsndfile
+              systemd
+              gettext
+              json-glib
+              gtk4
+            ];
+
+            postInstall = ''
+              # install udev/hwdb rules
+              mkdir -p $out/etc/udev/rules.d/
+              mkdir -p $out/etc/udev/hwdb.d/
+              cp ./udev/*.hwdb $out/etc/udev/hwdb.d/
+              cp ./udev/*.rules $out/etc/udev/rules.d/
+
+            '';
           };
-
-          nativeBuildInputs = with pkgs; [
-            pkg-config
-            autoreconfHook
-            wrapGAppsHook
-          ];
-
-          buildInputs = with pkgs; [
-            libtool
-            libusb1
-            libjack2
-            libsamplerate
-            libsndfile
-            gettext
-            json-glib
-            gtk4
-          ];
-
-          postInstall = ''
-            # install udev/hwdb rules
-            mkdir -p $out/etc/udev/rules.d/
-            mkdir -p $out/etc/udev/hwdb.d/
-            cp ./udev/*.hwdb $out/etc/udev/hwdb.d/
-            cp ./udev/*.rules $out/etc/udev/rules.d/
-          '';
+          default = overwitch;
         };
-        default = overwitch;
-      };
 
-      #### Dev shell (for `nix develop`)
-      devShells.default = pkgs.mkShell {
-        inputsFrom = [ self.packages ];
-      };
+        #### Dev shell (for `nix develop`)
+        devShells.default = pkgs.mkShell {
+          inputsFrom = [ self.packages ];
+        };
 
-  }) // 
-  {
+      }
+    )
+    // {
       #### NixOS module (`programs.overwitch`)
-      nixosModules.default = { config, lib, pkgs, ...}:
-      let cfg = config.services.overwitch;
-      in {
+      nixosModules.default =
+        {
+          config,
+          lib,
+          pkgs,
+          ...
+        }:
+        let
+          cfg = config.services.overwitch;
+        in
+        {
           options.services.overwitch = {
-              enable = lib.mkEnableOption "Enables Overwitch";
-
-	      dbus.enable = lib.mkOption {
-	        type = lib.types.bool;
-		default = true;
-	        description = "Enables dbus service";
-	      };
+            enable = lib.mkEnableOption "Enables Overwitch";
           };
 
           config = lib.mkIf cfg.enable {
+
+            # Actually install the package
             environment.systemPackages = [ self.packages.${pkgs.system}.overwitch ];
+
+            # Install the udev hwdb and rules
             services.udev.packages = [ self.packages.${pkgs.system}.overwitch ];
+
+            # DBus activation has replaced systemd (user) service
             services.dbus.packages = [ self.packages.${pkgs.system}.overwitch ];
-
-	    systemd.services = {
-	      overwitch-dbus = lib.mkIf cfg.dbus.enable {
-	        description = "Overwitch D-Bus Service";
-		serviceConfig = {
-		  Type = "dbus";
-		  BusName = "io.github.dagargo.OverwitchService";
-		  ExecStart = "${cfg.package}/bin/overwitch-service";
-		  Restart = "on-failure";
-		};
-
-		aliases = [ "io.github.dagargo.OverwitchService.service" ];
-	      };
-	    };
 
           };
         };
-      };
+    };
 }
